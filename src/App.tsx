b75f0dd3aa208ecc3e5ddd0623562678c4f56710
student <romanilichev@mail.ru>
2019-07-05 23:03:58 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
