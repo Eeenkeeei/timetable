cd2b23d162882aa88fe19aa77c20045699b4e27c
student <romanilichev@mail.ru>
2019-07-08 01:14:45 +0300

    Build

--- build/precache-manifest.605f988544b663285da297e636a4d6e8.js
+++ build/precache-manifest.605f988544b663285da297e636a4d6e8.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4bc32fe6cb05394a5b4ff12abae523dc",
+    "revision": "fd2d1d06b08375eca458f7687e1888ae",
     "url": "/index.html"
   },
   {
-    "revision": "1bff6980585f62e84041",
+    "revision": "ba6f43e83dc17b422437",
     "url": "/static/css/main.1d03c510.chunk.css"
   },
   {
-    "revision": "4a69852515a50ac90ac3",
-    "url": "/static/js/2.469fd70d.chunk.js"
+    "revision": "a287fe0bb18f551db3da",
+    "url": "/static/js/2.4a5f0548.chunk.js"
   },
   {
-    "revision": "1bff6980585f62e84041",
-    "url": "/static/js/main.0547e2c6.chunk.js"
+    "revision": "ba6f43e83dc17b422437",
+    "url": "/static/js/main.318a38dc.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
