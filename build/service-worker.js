26bb1e49da5b11895dfe70fdc67c23e69c9aeb23
student <romanilichev@mail.ru>
2019-07-09 00:29:41 +0300

    Фикс урла

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b1c733ef5736ac32a9a88f1e415ffd69.js"
+  "/precache-manifest.18e6ebe2e33f592fd3f7c959e0ff14b0.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/AppConstants.tsx
+++ src/AppConstants.tsx
@@ -1,2 +1,2 @@
-export const serverApi = 'http://192.168.0.122:7777';
+export const serverApi = 'https://react-timetable-server.herokuapp.com';
 // https://react-timetable-server.herokuapp.com
